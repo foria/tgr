a94f3bde38a855504577a890f4e9dd437be4cfe7
Marco Melluso <foriaa@gmail.com>
2018-07-17 16:10:17 -0400

    header and footer mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,41 @@
 // Header on scroll
 var prevScrollpos = window.pageYOffset;
+var topbarHeight_D = "-56px";
+var topbarHeight_M = "-25px";
+
+// Scroll to top functions
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        document.getElementById("backtotop").style.display = "block";
+    } else {
+        document.getElementById("backtotop").style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    $('html, body').animate({scrollTop: 0}, 500);
+}
+
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
+    if (currentScrollPos == 0) {
+        document.getElementById("header").className = "d-transparent";
+    }
   } else {
-    document.getElementById("header").style.top = "-56px";
+    document.getElementById("header").className = "";
+
+    if (window.innerWidth > 991) {
+        document.getElementById("header").style.top = topbarHeight_D;
+    } else {
+        document.getElementById("header").style.top = topbarHeight_M;
+    }
   }
   prevScrollpos = currentScrollPos;
+
+  scrollFunction();
 }
 
 $(document).ready(function(){
