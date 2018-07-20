cd288384449db4806a49b4cc4e53ea4abc94d906
Marco Melluso <foriaa@gmail.com>
2018-07-20 19:14:22 -0400

    added tinder swipe card js library and animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -71,3 +71,30 @@ $(document).ready(function(){
     }
 
 })
+
+if ( window.innerWidth < lg_bp ) {
+  document.addEventListener('DOMContentLoaded', function () {
+      var stack;
+
+      stack = window.swing.Stack();
+
+      [].forEach.call(document.querySelectorAll('.stack > div'), function (targetElement) {
+          stack.createCard(targetElement);
+
+          targetElement.classList.add('in-deck');
+      });
+
+      stack.on('throwout', function (e) {
+          console.log(e.target.innerText || e.target.textContent, 'has been thrown out of the stack to the', e.throwDirection, 'direction.');
+
+          //e.target.classList.remove('in-deck');
+          e.target.remove();
+      });
+
+      stack.on('throwin', function (e) {
+          console.log(e.target.innerText || e.target.textContent, 'has been thrown into the stack from the', e.throwDirection, 'direction.');
+
+          e.target.classList.add('in-deck');
+      });
+  });
+}
